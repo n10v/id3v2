4af1591f06f44016c283ae8b38e9cf06a8097147
bogem <albertnigma@gmail.com>
2016-05-25 18:48:30 +0200

    Cleanup in frame.go Delete Size() from Framer interface and move FormFrameHeader to tag.go

--- frame/frame.go
+++ frame/frame.go
@@ -2,7 +2,6 @@ package frame
 
 import (
 	"bytes"
-	"github.com/bogem/id3v2/util"
 	"sync"
 )
 
@@ -14,24 +13,4 @@ type Framer interface {
 	Form() []byte
 	ID() string
 	SetID(string)
-}
-
-type FrameHeader struct {
-	ID        string
-	FrameSize uint32
-}
-
-func FormFrameHeader(f Framer, size uint32) ([]byte, error) {
-	b := bytesBufPool.Get().(*bytes.Buffer)
-	b.Reset()
-	b.WriteString(f.ID())
-	if size, err := util.FormSize(size); err != nil {
-		return nil, err
-	} else {
-		b.Write(size)
-	}
-	b.WriteByte(0)
-	b.WriteByte(0)
-	bytesBufPool.Put(b)
-	return b.Bytes(), nil
 }
