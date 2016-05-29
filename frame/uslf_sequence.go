42ab472aa5eb6deb332800c2b3ab284875b1ed42
bogem <albertnigma@gmail.com>
2016-05-29 23:27:19 +0200

    Refactoring in USLFSequence code

--- frame/uslf_sequence.go
+++ frame/uslf_sequence.go
@@ -3,10 +3,33 @@ package frame
 type USLFSequencer interface {
 	Sequencer
 
-	USLF(id string, contentDescriptor string) UnsynchronisedLyricsFrame
-	AddUSLF(uslf UnsynchronisedLyricsFrame)
+	USLF(language string, contentDescriptor string) UnsynchronisedLyricsFramer
+	AddUSLF(UnsynchronisedLyricsFramer)
 }
 
 type USLFSequence struct {
 	sequence map[string]UnsynchronisedLyricsFramer
+}
+
+func NewUSLFSequence() *USLFSequence {
+	return &USLFSequence{
+		sequence: make(map[string]UnsynchronisedLyricsFramer),
+	}
+}
+
+func (us USLFSequence) Frames() []Framer {
+	frames := []Framer{}
+	for _, f := range us.sequence {
+		frames = append(frames, f)
+	}
+	return frames
+}
+
+func (us USLFSequence) USLF(language string, contentDescriptor string) UnsynchronisedLyricsFramer {
+	return us.sequence[language+contentDescriptor]
+}
+
+func (us *USLFSequence) AddUSLF(uslf UnsynchronisedLyricsFramer) {
+	id := uslf.Language() + uslf.ContentDescriptor()
+	us.sequence[id] = uslf
 }
