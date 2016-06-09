6677171a86488698ca628aa83041f4f250cbf575
bogem <albertnigma@gmail.com>
2016-06-10 01:08:33 +0500

    Cut key of USLT in V24CommonIDs map

--- frame/v4.go
+++ frame/v4.go
@@ -2,13 +2,13 @@ package frame
 
 var (
 	V24CommonIDs = map[string]string{
-		"Title":                      "TIT2",
-		"Artist":                     "TPE1",
-		"Album":                      "TALB",
-		"Year":                       "TYER",
-		"Genre":                      "TCON",
-		"Attached Picture":           "APIC",
-		"Unsynchronised Lyrics/Text": "USLT",
-		"Comment":                    "COMM",
+		"Title":            "TIT2",
+		"Artist":           "TPE1",
+		"Album":            "TALB",
+		"Year":             "TYER",
+		"Genre":            "TCON",
+		"Attached picture": "APIC",
+		"USLT":             "USLT",
+		"Comment":          "COMM",
 	}
 )
