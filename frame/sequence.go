82d30addc5b6bdbe27f8c21fca830274d909d6c6
bogem <albertnigma@gmail.com>
2016-06-10 11:22:19 +0500

    Make clearer comment in Sequencer interface

--- frame/sequence.go
+++ frame/sequence.go
@@ -1,7 +1,7 @@
 package frame
 
-// Interface Sequence is used to manipulate with frames, which can be used more
-// than one time (e.g. APIC, COMM, USLT, SYLT and etc.)
+// Interface Sequence is used to manipulate with frames, which can be in tag
+// more than one (e.g. APIC, COMM, USLT, SYLT and etc.)
 type Sequencer interface {
 	AddFrame(Framer)
 	Frames() []Framer
