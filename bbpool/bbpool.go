7281ef073ee0c6b682e6f9c1eb22870bcbc1712d
bogem <albertnigma@gmail.com>
2016-09-03 22:06:29 +0500

    Add license notice in bbpool.go

--- bbpool/bbpool.go
+++ bbpool/bbpool.go
@@ -1,3 +1,7 @@
+// Copyright 2016 Albert Nigmatzianov. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 // Package bbpool is used only for internal usage. Users of
 // library id3v2 must not use it.
 package bbpool
