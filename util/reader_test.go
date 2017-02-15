7f9bad87f67263ce91c7e8603c7a21e93b6480df
bogem <albertnigma@gmail.com>
2017-02-15 22:21:13 +0100

    Add more tests to reader

--- util/reader_test.go
+++ util/reader_test.go
@@ -13,41 +13,77 @@ var (
 	bs = []byte{0, 11, 22, 33, 44, 55, 77, 88, 55, 55, 66, 77, 88}
 )
 
+func TestReadTillDelim(t *testing.T) {
+	bsReader := NewReader(bytes.NewReader(bs))
+
+	firstIndexOf55 := bytes.Index(bs, []byte{55})
+	if firstIndexOf55 < 0 {
+		t.Fatal("Can't find 55 in bs")
+	}
+	expected := bs[:firstIndexOf55]
+
+	read, err := bsReader.ReadTillDelim(55)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(expected, read) {
+		t.Errorf("Expected: %v, got: %v", expected, read)
+	}
+	if len(bs)-len(expected) != bsReader.buf.Buffered() {
+		t.Errorf("Expected buffered: %v, got: %v", len(bs)-len(expected), bsReader.buf.Buffered())
+	}
+}
+
+func TestReadTillZero(t *testing.T) {
+	bsReader := NewReader(bytes.NewReader(bs))
+
+	read, err := bsReader.ReadTillDelim(0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal([]byte{}, read) {
+		t.Errorf("Expected empty buffer, got %v", read)
+	}
+	if len(bs) != bsReader.buf.Buffered() {
+		t.Errorf("Expected buffered: %v, got: %v", len(bs), bsReader.buf.Buffered())
+	}
+}
+
 func TestReadSeveralBytes(t *testing.T) {
-	bsRd := bytes.NewReader(bs)
-	bsReader := NewReader(bsRd)
+	bsReader := NewReader(bytes.NewReader(bs))
 	n := 5 // Read 5 elements
 
 	read, err := bsReader.ReadSeveralBytes(n)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	if n != len(read) {
-		t.Errorf("Expecting to read: %v, got: %v", n, read)
+		t.Errorf("Expected: %v, got: %v", n, read)
 	}
 	if !bytes.Equal(bs[:n], read) {
-		t.Errorf("Expecting: %v, got: %v", bs[:n], len(read))
+		t.Errorf("Expected: %v, got: %v", bs[:n], len(read))
 	}
-
-	if bsReader.buf.Buffered() != len(bs)-n {
-		t.Errorf("Expecting buffered: %v, got: %v", len(bs)-n, bsReader.buf.Buffered())
+	if len(bs)-n != bsReader.buf.Buffered() {
+		t.Errorf("Expected buffered: %v, got: %v", len(bs)-n, bsReader.buf.Buffered())
 	}
 }
 
 func TestReadTillDelims(t *testing.T) {
-	bsRd := bytes.NewReader(bs)
-	bsReader := NewReader(bsRd)
+	bsReader := NewReader(bytes.NewReader(bs))
 	delims := []byte{55, 66}
 	expectedLen := 9
 
 	read, err := bsReader.ReadTillDelims(delims)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	if expectedLen != len(read) {
-		t.Errorf("Expecting to read: %v, got: %v", expectedLen, len(read))
+		t.Errorf("Expected: %v, got: %v", expectedLen, len(read))
 	}
 	if !bytes.Equal(bs[:expectedLen], read) {
-		t.Errorf("Expecting: %v, got: %v", bs[:expectedLen], read)
+		t.Errorf("Expected: %v, got: %v", bs[:expectedLen], read)
+	}
+	if len(bs)-len(read) != bsReader.buf.Buffered() {
+		t.Errorf("Expected buffered: %v, got: %v", len(bs)-len(read), bsReader.buf.Buffered())
 	}
 }
