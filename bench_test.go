966cc686e8c884f23ec5772baf5a2cdec6ce80a4
Albert Nigmatzianov <albertnigma@gmail.com>
2017-05-19 19:09:17 +0200

    Add BenchmarkReuseTag

--- bench_test.go
+++ bench_test.go
@@ -31,14 +31,16 @@ func BenchmarkParseAndWriteCommonCase(b *testing.B) {
 	if err != nil {
 		b.Error("Error while reading front cover file")
 	}
+	b.ResetTimer()
 
-	// We use b.N+1, because in first iteration we just resetting tag
-	// and setting common frames. Also timer will be resetted.
+	// We use b.N+1, because in first iteration we just reset tag
+	// and set common frames. Also timer will be resetted.
 	for n := 0; n < b.N+1; n++ {
 		tag, err := Open(mp3Name, defaultOpts)
 		if tag == nil || err != nil {
 			b.Fatal("Error while opening mp3 file:", err)
 		}
+		defer tag.Close()
 
 		// Delete all frames in first iteration
 		if n == 0 {
@@ -61,9 +63,6 @@ func BenchmarkParseAndWriteCommonCase(b *testing.B) {
 
 		if err = tag.Save(); err != nil {
 			b.Error("Error while saving a tag:", err)
-		}
-		if err = tag.Close(); err != nil {
-			b.Error("Error while closing a tag:", err)
 		}
 
 		// Reset timer because we just resetting tag in first iteration
@@ -78,16 +77,18 @@ func BenchmarkParseAndWriteManyFrames(b *testing.B) {
 	if err != nil {
 		b.Error("Error while reading front cover file")
 	}
+	b.ResetTimer()
 
-	// We use b.N+1, because in first iteration we just resetting tag
-	// and setting many frames. Also timer will be resetted.
+	// We use b.N+1, because in first iteration we just reset tag
+	// and set many frames. Also timer will be resetted.
 	for n := 0; n < b.N+1; n++ {
 		tag, err := Open(mp3Name, defaultOpts)
 		if tag == nil || err != nil {
 			b.Fatal("Error while opening mp3 file:", err)
 		}
+		defer tag.Close()
 
-		// Delete all frames in first iteration
+		// Delete all frames in first iteration.
 		if n == 0 {
 			tag.DeleteAllFrames()
 		}
@@ -98,7 +99,7 @@ func BenchmarkParseAndWriteManyFrames(b *testing.B) {
 		tag.SetYear("2016")
 		tag.SetGenre("Genre")
 
-		// Set front cover
+		// Set front cover.
 		pic := PictureFrame{
 			Encoding:    ENUTF8,
 			MimeType:    "image/jpeg",
@@ -108,7 +109,7 @@ func BenchmarkParseAndWriteManyFrames(b *testing.B) {
 		}
 		tag.AddAttachedPicture(pic)
 
-		// Set USLT
+		// Set USLT.
 		uslt := UnsynchronisedLyricsFrame{
 			Encoding:          ENUTF8,
 			Language:          "eng",
@@ -117,7 +118,7 @@ func BenchmarkParseAndWriteManyFrames(b *testing.B) {
 		}
 		tag.AddUnsynchronisedLyricsFrame(uslt)
 
-		// Set comment
+		// Set comment.
 		comm := CommentFrame{
 			Encoding:    ENUTF8,
 			Language:    "eng",
@@ -129,13 +130,57 @@ func BenchmarkParseAndWriteManyFrames(b *testing.B) {
 		if err = tag.Save(); err != nil {
 			b.Error("Error while saving a tag:", err)
 		}
-		if err = tag.Close(); err != nil {
-			b.Error("Error while closing a tag:", err)
-		}
 
-		// Reset timer because we just resetting tag in first iteration
+		// Reset timer because we just reset file in first iteration.
 		if n == 0 {
 			b.ResetTimer()
 		}
+	}
+}
+
+func BenchmarkReuseTag(b *testing.B) {
+	frontCover, err := ioutil.ReadFile(frontCoverName)
+	if err != nil {
+		b.Error("Error while reading front cover file")
+	}
+	tag := NewEmptyTag()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		tag.DeleteAllFrames()
+
+		tag.SetTitle("Title")
+		tag.SetArtist("Artist")
+		tag.SetAlbum("Album")
+		tag.SetYear("2016")
+		tag.SetGenre("Genre")
+
+		// Set front cover.
+		pic := PictureFrame{
+			Encoding:    ENUTF8,
+			MimeType:    "image/jpeg",
+			PictureType: PTFrontCover,
+			Description: "Front cover",
+			Picture:     frontCover,
+		}
+		tag.AddAttachedPicture(pic)
+
+		// Set USLT.
+		uslt := UnsynchronisedLyricsFrame{
+			Encoding:          ENUTF8,
+			Language:          "eng",
+			ContentDescriptor: "Content descriptor",
+			Lyrics:            "bogem/id3v2",
+		}
+		tag.AddUnsynchronisedLyricsFrame(uslt)
+
+		// Set comment.
+		comm := CommentFrame{
+			Encoding:    ENUTF8,
+			Language:    "eng",
+			Description: "Short description",
+			Text:        "The actual text",
+		}
+		tag.AddCommentFrame(comm)
 	}
 }
