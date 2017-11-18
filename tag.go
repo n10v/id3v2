2d9b4fbdd0bb0d08592fcce8030e2b538c11dbe4
Albert Nigmatzianov <albertnigma@gmail.com>
2017-11-18 15:38:54 +0100

    Use 96KB byte slice in Save

--- tag.go
+++ tag.go
@@ -324,7 +324,7 @@ func (tag *Tag) Save() error {
 	}
 
 	// Write to new file the music part.
-	buf := getByteSlice(32 * 1024)
+	buf := getByteSlice(96 * 1024)
 	defer putByteSlice(buf)
 	if _, err = io.CopyBuffer(newFile, originalFile, buf); err != nil {
 		return err
