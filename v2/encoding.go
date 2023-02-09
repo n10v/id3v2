997e77f91d8db331eceff756f8890b9e708fbb38
Albert Nigmatzianov <albertnigma@gmail.com>
2023-02-09 17:44:26 +0100

    Revert bytes.ReplaceAll for backward-compatibility with Go 1.11 (#78)

--- v2/encoding.go
+++ v2/encoding.go
@@ -113,7 +113,9 @@ func decodeText(src []byte, from Encoding) string {
 	// See https://apps.timwhitlock.info/unicode/inspect?s=%EF%BF%BD.
 	// See https://en.wikipedia.org/wiki/Byte_order_mark#UTF-8.
 	if from.Equals(EncodingUTF16) {
-		result = bytes.ReplaceAll(result, []byte{0xEF, 0xBF, 0xBD}, []byte{})
+		// bytes.Replace(s, old, new, -1) is the same as bytes.ReplaceAll(s, old, new),
+		// but bytes.ReplaceAll is only added in Go 1.12.
+		result = bytes.Replace(result, []byte{0xEF, 0xBF, 0xBD}, []byte{}, -1)
 	}
 
 	return string(result)
